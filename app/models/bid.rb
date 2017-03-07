235f92e16653377e34f739fa9c553bd3ca96502e
Jason Jeffries <jjeffries@blenderbox.com>
2017-03-07 14:51:39 -0500

    order

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,10 +1,18 @@
 class Bid < ActiveRecord::Base
   belongs_to :auction
-  belongs_to :bidder, class_name: 'User'
+  belongs_to  :bidder, class_name: 'User'
 
   validates :amount, presence: true
   validates :auction, presence: true
   validates :bidder, presence: true
+
+  def self.default_scope
+    joins(:bidder).order('users.is_mwbe DESC')
+  end
+
+  def bidder_mwbe
+    bidder.is_mwbe
+  end
 
   def bidder_name
     bidder.name
@@ -13,4 +21,10 @@ class Bid < ActiveRecord::Base
   def decorated_bidder
     UserPresenter.new(bidder)
   end
+
+    # class << self
+  #   def featured
+  #     where(featured: true).limit(3)
+  #   end
+  # end
 end
