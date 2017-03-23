fd5a143bfd530e71c1d8b20dfa3921278c73b0dc
Jason Jeffries <jjeffries@blenderbox.com>
2017-03-23 14:23:10 -0400

    commenting out /api docs

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   get '/sign_up', to: 'sign_ups#show'
   get '/sign_in', to: 'sign_ins#show'
 
-  get '/api' => 'docs#index', as: 'api_doc'
+  #get '/api' => 'docs#index', as: 'api_doc'
 
   namespace :admin do
     resources :auction_reports, only: [:show]
