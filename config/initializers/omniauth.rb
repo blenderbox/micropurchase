6aca6ad5df050600bdf7c052bd31c4808facf6b9
jasonblend <jjeffries@blenderbox.com>
2017-05-24 15:06:33 -0400

    Update omniauth.rb
    overriding OmniAuth.full_host to use load balancer host rather than Application host.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OmniAuth.config.full_host = lambda do |env|
+    forwarded_host = env['ROOT_URL']
+  end
   provider(
     :github,
     GithubCredentials.client_id,
