5eacb5ed05273a3f0f505896c2f3179e1dcb11c1
jasonblend <jjeffries@blenderbox.com>
2017-06-20 11:03:35 -0400

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_micropurchase_session', secure: true
+Micropurchase::Application.config.session_store :cookie_store, key: '_micropurchase_session', secure: true
