fe50f0bfd3dedc51c4552487f27b4665ff29eadb
jasonblend <jjeffries@blenderbox.com>
2017-06-21 14:49:15 -0400

    Update rejected.rb

--- app/presenters/fms_status_presenter/rejected.rb
+++ app/presenters/fms_status_presenter/rejected.rb
@@ -16,7 +16,7 @@ class FmsStatusPresenter::Rejected < FmsStatusPresenter::Base
   end
 
   def message
-    "Your FNS Vendor Number was not found in nyc checkbox.gov. Please enter a valid FMS Vendor Number
+    "Your FNS Vendor Number was not found in the City's Payee Information Portal (PIP). Please enter a valid FMS Vendor Number
     to complete your profile. Check the city's Payee Information Portal (PIP) at 
     https://a127-pip.nyc.gov/webapp/PRDPCW/SelfService to make sure
     your FMS number is correct. If you need any help contact us at http://www1.nyc.gov/site/forward/contact/contact.page"
